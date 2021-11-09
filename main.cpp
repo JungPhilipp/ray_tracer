d532153643ad7890842f6706738115b3e70b36bf
Philipp Jung <philipp.jung@mailbox.org>
2021-11-09 09:45:13 +0100

    Use uniform initializatioN

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
-#include <math.h>   // smallpt, a Path Tracer by Kevin Beason, 2008
-#include <stdio.h>  //        Remove "-fopenmp" for g++ version < 4.2
-#include <stdlib.h> // Make : g++ -O3 -fopenmp smallpt.cpp -o smallpt
+// smallpt, a Path Tracer by Kevin Beason, 2008
+//        Remove "-fopenmp" for g++ version < 4.2
+// Make : g++ -O3 -fopenmp smallpt.cpp -o smallpt
+// Usage: time ./smallpt 5000 && xv image.ppm
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 struct Vec
-{                   // Usage: time ./smallpt 5000 && xv image.ppm
-    double x, y, z; // position, also color (r,g,b)
-    Vec(double x_ = 0, double y_ = 0, double z_ = 0)
-    {
-        x = x_;
-        y = y_;
-        z = z_;
-    }
-    Vec operator+(const Vec& b) const { return Vec(x + b.x, y + b.y, z + b.z); }
-    Vec operator-(const Vec& b) const { return Vec(x - b.x, y - b.y, z - b.z); }
-    Vec operator*(double b) const { return Vec(x * b, y * b, z * b); }
-    Vec mult(const Vec& b) const { return Vec(x * b.x, y * b.y, z * b.z); }
+{
+    // position, also color (r,g,b)
+    double x = 0;
+    double y = 0;
+    double z = 0;
+    Vec operator+(const Vec& b) const { return Vec{ x + b.x, y + b.y, z + b.z }; }
+    Vec operator-(const Vec& b) const { return Vec{ x - b.x, y - b.y, z - b.z }; }
+    Vec operator*(double b) const { return Vec{ x * b, y * b, z * b }; }
+    Vec mult(const Vec& b) const { return Vec{ x * b.x, y * b.y, z * b.z }; }
     Vec& norm() { return *this = *this * (1 / sqrt(x * x + y * y + z * z)); }
     double dot(const Vec& b) const { return x * b.x + y * b.y + z * b.z; } // cross:
     Vec operator%(Vec& b)
     {
-        return Vec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x);
+        return Vec{ y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x };
     }
 };
 struct Ray
@@ -30,7 +32,7 @@ struct Ray
     {
     }
 };
-enum Refl_t
+enum class Refl_t
 {
     DIFF,
     SPEC,
@@ -40,7 +42,7 @@ struct Sphere
 {
     double rad;  // radius
     Vec p, e, c; // position, emission, color
-    Refl_t refl; // reflection type (DIFFuse, SPECular, REFRactive)
+    Refl_t refl; // reflection type (DIFFuse,Refl_t::SPECular,Refl_t::REFRactive)
     Sphere(double rad_, Vec p_, Vec e_, Vec c_, Refl_t refl_)
       : rad(rad_)
       , p(p_)
@@ -62,15 +64,31 @@ struct Sphere
 };
 Sphere spheres[] = {
     // Scene: radius, position, emission, color, material
-    Sphere(1e5, Vec(1e5 + 1, 40.8, 81.6), Vec(), Vec(.75, .25, .25), DIFF),   // Left
-    Sphere(1e5, Vec(-1e5 + 99, 40.8, 81.6), Vec(), Vec(.25, .25, .75), DIFF), // Rght
-    Sphere(1e5, Vec(50, 40.8, 1e5), Vec(), Vec(.75, .75, .75), DIFF),         // Back
-    Sphere(1e5, Vec(50, 40.8, -1e5 + 170), Vec(), Vec(), DIFF),               // Frnt
-    Sphere(1e5, Vec(50, 1e5, 81.6), Vec(), Vec(.75, .75, .75), DIFF),         // Botm
-    Sphere(1e5, Vec(50, -1e5 + 81.6, 81.6), Vec(), Vec(.75, .75, .75), DIFF), // Top
-    Sphere(16.5, Vec(27, 16.5, 47), Vec(), Vec(1, 1, 1) * .999, SPEC),        // Mirr
-    Sphere(16.5, Vec(73, 16.5, 78), Vec(), Vec(1, 1, 1) * .999, REFR),        // Glas
-    Sphere(600, Vec(50, 681.6 - .27, 81.6), Vec(12, 12, 12), Vec(), DIFF)     // Lite
+    Sphere(1e5,
+           Vec{ 1e5 + 1, 40.8, 81.6 },
+           Vec{},
+           Vec{ .75, .25, .25 },
+           Refl_t::DIFF), // Left
+    Sphere(1e5,
+           Vec{ -1e5 + 99, 40.8, 81.6 },
+           Vec{},
+           Vec{ .25, .25, .75 },
+           Refl_t::DIFF),                                                         // Rght
+    Sphere(1e5, Vec{ 50, 40.8, 1e5 }, Vec(), Vec{ .75, .75, .75 }, Refl_t::DIFF), // Back
+    Sphere(1e5, Vec{ 50, 40.8, -1e5 + 170 }, Vec{}, Vec(), Refl_t::DIFF),         // Frnt
+    Sphere(1e5, Vec{ 50, 1e5, 81.6 }, Vec(), Vec{ .75, .75, .75 }, Refl_t::DIFF), // Botm
+    Sphere(1e5,
+           Vec{ 50, -1e5 + 81.6, 81.6 },
+           Vec{},
+           Vec{ .75, .75, .75 },
+           Refl_t::DIFF),                                                          // Top
+    Sphere(16.5, Vec{ 27, 16.5, 47 }, Vec{}, Vec{ 1, 1, 1 } * .999, Refl_t::SPEC), // Mirr
+    Sphere(16.5, Vec{ 73, 16.5, 78 }, Vec{}, Vec{ 1, 1, 1 } * .999, Refl_t::REFR), // Glas
+    Sphere(600,
+           Vec{ 50, 681.6 - .27, 81.6 },
+           Vec{ 12, 12, 12 },
+           Vec(),
+           Refl_t::DIFF) // Lite
 };
 inline double
 clamp(double x)
@@ -106,20 +124,22 @@ radiance(const Ray& r, int depth, unsigned short* Xi)
         f = obj.c;
     double p = f.x > f.y && f.x > f.z ? f.x : f.y > f.z ? f.y : f.z; // max refl
     if (++depth > 5)
+    {
         if (erand48(Xi) < p)
             f = f * (1 / p);
         else
             return obj.e; // R.R.
-    if (obj.refl == DIFF)
-    { // Ideal DIFFUSE reflection
+    }
+    if (obj.refl == Refl_t::DIFF)
+    { // IdealRefl_t::DIFFUSE reflection
         double r1 = 2 * M_PI * erand48(Xi), r2 = erand48(Xi), r2s = sqrt(r2);
-        Vec w = nl, u = ((fabs(w.x) > .1 ? Vec(0, 1) : Vec(1)) % w).norm(), v = w % u;
+        Vec w = nl, u = ((fabs(w.x) > .1 ? Vec{ 0, 1 } : Vec{ 1 }) % w).norm(), v = w % u;
         Vec d = (u * cos(r1) * r2s + v * sin(r1) * r2s + w * sqrt(1 - r2)).norm();
         return obj.e + f.mult(radiance(Ray(x, d), depth, Xi));
     }
-    else if (obj.refl == SPEC) // Ideal SPECULAR reflection
+    else if (obj.refl == Refl_t::SPEC) // IdealRefl_t::SPECULAR reflection
         return obj.e + f.mult(radiance(Ray(x, r.d - n * 2 * n.dot(r.d)), depth, Xi));
-    Ray reflRay(x, r.d - n * 2 * n.dot(r.d)); // Ideal dielectric REFRACTION
+    Ray reflRay(x, r.d - n * 2 * n.dot(r.d)); // Ideal dielectricRefl_t::REFRACTION
     bool into = n.dot(nl) > 0;                // Ray from outside going in?
     double nc = 1, nt = 1.5, nnt = into ? nc / nt : nt / nc, ddn = r.d.dot(nl), cos2t;
     if ((cos2t = 1 - nnt * nnt * (1 - ddn * ddn)) < 0) // Total internal reflection
@@ -140,8 +160,9 @@ int
 main(int argc, char* argv[])
 {
     int w = 1024, h = 768, samps = argc == 2 ? atoi(argv[1]) / 4 : 1; // # samples
-    Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).norm());        // cam pos, dir
-    Vec cx = Vec(w * .5135 / h), cy = (cx % cam.d).norm() * .5135, r, *c = new Vec[w * h];
+    Ray cam(Vec{ 50, 52, 295.6 }, Vec{ 0, -0.042612, -1 }.norm());    // cam pos, dir
+    Vec cx = Vec{ w * .5135 / h }, cy = (cx % cam.d).norm() * .5135, r,
+        *c = new Vec[w * h];
 #pragma omp parallel for schedule(dynamic, 1) private(r) // OpenMP
     for (int y = 0; y < h; y++)
     { // Loop over image rows
@@ -161,7 +182,7 @@ main(int argc, char* argv[])
                         r = r + radiance(Ray(cam.o + d * 140, d.norm()), 0, Xi) *
                                   (1. / samps);
                     } // Camera rays are pushed ^^^^^ forward to start in interior
-                    c[i] = c[i] + Vec(clamp(r.x), clamp(r.y), clamp(r.z)) * .25;
+                    c[i] = c[i] + Vec{ clamp(r.x), clamp(r.y), clamp(r.z) } * .25;
                 }
     }
     FILE* f = fopen("image.ppm", "w"); // Write image to PPM file.
